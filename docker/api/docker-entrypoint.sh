5b50574adcb797cdc6ffc3521dbc78fa0fe949c1
Mark Ormesher <me@markormesher.co.uk>
2021-12-29 15:30:43 +0000

    remove postgres replication, remove some useless scripts

--- docker/api/docker-entrypoint.sh
+++ docker/api/docker-entrypoint.sh
@@ -2,8 +2,6 @@
 set -euo pipefail
 
 ./node_modules/.bin/wait-for-it postgres_primary:5432 --timeout=30 --strict &
-./node_modules/.bin/wait-for-it postgres_replica_1:5432 --timeout=30 --strict &
-./node_modules/.bin/wait-for-it postgres_replica_2:5432 --timeout=30 --strict &
 ./node_modules/.bin/wait-for-it redis:6379 --timeout=30 --strict &
 
 wait
