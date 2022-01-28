3bd510dc5386aac3655bb3589320f5b988d6b819
Mark Ormesher <me@markormesher.co.uk>
2022-01-28 17:53:32 +0000

    add context for private packages

--- src/middleware/auth-middleware.ts
+++ src/middleware/auth-middleware.ts
@@ -1,11 +1,13 @@
 import { NextFunction, Request, RequestHandler, Response } from "express";
 import { getOrCreateUserWithExternalUsername } from "../managers/user-manager";
+import { logger } from "../utils/logging";
 import { StatusError } from "../utils/StatusError";
 
 const loadUser: RequestHandler = (req: Request, res: Response, next: NextFunction) => {
   const username = req.header("remote-user");
   const name = req.header("remote-name");
   if (!username || !name) {
+    logger.error("Username or name was missing from request", { headers: req.headers });
     throw new StatusError(401);
   }
 
